be49173c39fa047f9b492938c508d2f3bddda904
PoliTwit1984 <joe.wilson@live.com>
2022-01-13 10:27:01 -0600

    cleanup

--- streaming.py
+++ streaming.py
@@ -1,11 +1,31 @@
 import tweepy
 
-stream = tweepy.Stream(consumer_key='anvCaAUUuYqG3xLR7dNAzR0dr', 
-                       consumer_secret='ULtEkEdycJzC08cyebSg1ExdOVgCg5pUG0qtLPnLta24ClNDqr', 
-                       access_token='1469761271709028359-FrJt9sku3toFacDx1tdvmSPc273pKf', 
-                       access_token_secret='mlblwNEQ21A6IrmC4boX42Rctpq0WLNMfTsoOSmOwPrAa')
+API_KEY = "anvCaAUUuYqG3xLR7dNAzR0dr"
+API_SECRET_KEY = "ULtEkEdycJzC08cyebSg1ExdOVgCg5pUG0qtLPnLta24ClNDqr"
+ACCESS_TOKEN = "1469761271709028359-FrJt9sku3toFacDx1tdvmSPc273pKf"
+ACCESS_TOKEN_SECRET = "mlblwNEQ21A6IrmC4boX42Rctpq0WLNMfTsoOSmOwPrAa"
+
+stream = tweepy.Stream(
+  API_KEY, API_SECRET_KEY,
+  ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+)
 
 
-stream.sample()
+
+class IDPrinter(tweepy.Stream):
+
+    def on_status(self, status):
+        print(status.id)
+        print(status.text)
+        print(status.entities)
+        print(status.user.screen_name)
+
+
+
+printer = IDPrinter(
+  API_KEY, API_SECRET_KEY,
+  ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+)
+printer.filter(track=['missouri teachers'],languages = ["en"])
 
 
