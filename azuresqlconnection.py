a2ff382ff2aee248c67e0e363de31b6d04b6d39d
PoliTwit1984 <joe.wilson@live.com>
2022-01-17 03:54:28 -0600

    stream saves to azure sql

--- azuresqlconnection.py
+++ azuresqlconnection.py
@@ -1,5 +1,14 @@
 import textwrap
 import pyodbc
+import dataset
+import datetime
+
+
+def has_value(cursor, table, column, value):
+    query = 'SELECT 1 from {} WHERE {} = ? LIMIT 1'.format(table, column)
+    return cursor.execute(query, (value,)).fetchone() is not None
+
+
 
 driver = '{ODBC Driver 17 for SQL Server}'
 
@@ -7,6 +16,8 @@ server_name = 'twitpoli1984-sqlsrv'
 database_name = 'mosenatetweets-db'
 
 server = '{server_name}.database.windows.net,1433'.format(server_name=server_name)
+
+tweet_text = "this is tweet text"
 
 username = "joewils"
 password = "Pissyduck113!@"
@@ -28,31 +39,33 @@ connection_string = textwrap.dedent('''
         password=password
 ))
 
+
+unid = 1234545645656356
+
 cnxn: pyodbc.Connection = pyodbc.connect(connection_string)
 crsr: pyodbc.Cursor = cnxn.cursor()
 
-# select_sql = "SELECT * From [mosenators]"
-# crsr.execute(select_sql)
-# print(crsr.fetchall())
+#insert_sql = "INSERT INTO TWEET_ALL_UP (tweet_created_at ,tweet_id, tweet_text, tweet_lang,tweet_source,tweet_reply_settings,tweet_conversation_id,tweet_in_repsonse_to_user_id,tweet_username,tweet_user_tweet_count,tweet_user_description,tweet_user_created_at,tweet_user_location,tweet_user_pinned_tweet,tweet_user_profile_url,tweet_user_verified,tweet_user_listed_count,tweet_user_following_count,tweet_user_followers_count,tweet_reply_count,tweet_like_count,tweet_quote_count,tweet_reference_type,tweet_reference_id,tweet_clean_text,tweet_sentiment_all,tweet_sentiment_compound,tweet_hashtags,tweet_urls,tweet_annotations,tweet_mentions,tweet_user_id,tweet_context_annotations,tweet_domain_ids,tweet_entity_ids) VALUES"
+
+#checking for tweet_id since it is primary key
+crsr.execute(
+    "SELECT tweet_id, COUNT(*) FROM tweet_all_up WHERE tweet_id = ? GROUP BY tweet_id",
+    (unid)
+)
+results = crsr.fetchall()
+row_count = crsr.rowcount
+# print("number of affected rows: {}".format(row_count))
+if row_count == 0:
+    print("It Does Not Exist")    
+    count = crsr.execute("""
+    INSERT INTO TWEET_ALL_UP (tweet_text, tweet_lang, tweet_id, tweet_user_listed_count) 
+    VALUES (?,?,?,?)""",
+    tweet_text, 'jp', 123456789443, 69).rowcount
 
 
-
-#insert_sql = "INSERT INTO TWEETS_ALL_UP (tweet_created_at ,tweet_id, tweet_text, tweet_lang,tweet_source,tweet_reply_settings,tweet_conversation_id,tweet_in_repsonse_to_user_id,tweet_username,tweet_user_tweet_count,tweet_user_description,tweet_user_created_at,tweet_user_location,tweet_user_pinned_tweet,tweet_user_profile_url,tweet_user_verified,tweet_user_listed_count,tweet_user_following_count,tweet_user_followers_count,tweet_reply_count,tweet_like_count,tweet_quote_count,tweet_reference_type,tweet_reference_id,tweet_clean_text,tweet_sentiment_all,tweet_sentiment_compound,tweet_hashtags,tweet_urls,tweet_annotations,tweet_mentions,tweet_user_id,tweet_context_annotations,tweet_domain_ids,tweet_entity_ids) VALUES"
-
-#insert_sql = "INSERT INTO [tweets_all_up] (Senator, District, Party, Followers, UserDescription, StatusCount, Location) VALUES (?, ?, ?, ?, ?, ?, ?)"
-
-# insert_sql = "INSERT INTO TWEETS_ALL_UP (tweet_created_at ,tweet_id, tweet_text) VALUES ()
-# records = [
-#     ('2022-01-11 21:44:06+00:00', 1466258632627130373, 'tweet text')
-#     ]
+print('Rows inserted: ' + str(count))
     
-crsr.execute("INSERT INTO TWEETS_ALL_UP (tweet_id, tweet_text) VALUES (%s, %s);", (1234567, "tweettext150"))
-#print("Inserted",cursor.rowcount,"row(s) of data.")
-
-
-
-#crsr.executemany(insert_sql, records)
-#crsr.commit()
+crsr.commit()
 
 cnxn.close()
 
--- streaming.py
+++ streaming.py
@@ -23,91 +23,243 @@
 # possibly_sensitive : Indicates whether the status is sensitive or not.
 # lang : The language of the status. 
 
+from sqlalchemy import null
 import tweepy
 import pandas as pd
 import csv
+import textwrap
+import pyodbc
+import datetime
+import preprocessor as p
+from nltk.tokenize import sent_tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+nltk.download('vader_lexicon')
 
 API_KEY = "anvCaAUUuYqG3xLR7dNAzR0dr"
 API_SECRET_KEY = "ULtEkEdycJzC08cyebSg1ExdOVgCg5pUG0qtLPnLta24ClNDqr"
 ACCESS_TOKEN = "1469761271709028359-FrJt9sku3toFacDx1tdvmSPc273pKf"
-ACCESS_TOKEN_SECRET = "mlblwNEQ21A6IrmC4boX42Rctpq0WLNMfTsoOSmOwPrAa"
-
-# tweet_column_names = ["tweet_id", "tweet_text", "tweet_entities", "tweet_source", "tweet_source_url", "tweet_in_reply_to_status_id",        "tweet_in_reply_to_user_id", "tweet_in_reply_to_screen_name", "tweet_username,", "tweet_user", "tweet_geo", "tweet_coordinates", "tweet_place", "tweet_coordinates", "tweet_is_quote_status", "tweet_retweet_count", "tweet_favorited_count", "tweet_favorited", "tweet_retweted", "tweet_lang", "tweet_created_at"]
-
-#df = pd.DataFrame()
-#df = pd.DataFrame(columns = tweet_column_names)
-        
+ACCESS_TOKEN_SECRET = "mlblwNEQ21A6IrmC4boX42Rctpq0WLNMfTsoOSmOwPrAa"      
         
 stream = tweepy.Stream(
   API_KEY, API_SECRET_KEY,
   ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 )
 
+
+
 class IDPrinter(tweepy.Stream):
 
-
     def on_status(self, status):
-      
-        # tweet_column_names = ["tweet_id", "tweet_text", "tweet_entities", "tweet_source", "tweet_source_url", "tweet_in_reply_to_status_id",        "tweet_in_reply_to_user_id", "tweet_in_reply_to_screen_name", "tweet_username,", "tweet_user", "tweet_geo", "tweet_coordinates", "tweet_place", "tweet_coordinates", "tweet_is_quote_status", "tweet_retweet_count", "tweet_favorited_count", "tweet_favorited", "tweet_retweeted", "tweet_lang", "tweet_created_at"]
         
-        #fields = [tweet_id, tweet_text, tweet_entities, tweet_source, tweet_source_url, tweet_in_reply_to_status_id, tweet_in_reply_to_user_id, tweet_in_reply_to_screen_name, tweet_username,, tweet_user, tweet_geo, tweet_coordinates, tweet_place, tweet_coordinates, tweet_is_quote_status, tweet_retweet_count, tweet_favorited_count, tweet_favorited, tweet_retweted, tweet_lang, tweet_created_at]
-
-        # df = pd.DataFrame()
-        # df = pd.DataFrame(columns = tweet_column_names)
+        ###########################################################################################################################
+        query = 'Covid lang:en'
+        project = 'Covid Stream test'
+        jobtype = "stream"
+        ###########################################################################################################################
         
         tweet_id = status.id
         tweet_text = status.text
-        tweet_entities = status.entities
+          
+        ent_dict = status.entities
+        #place_dict = status.place
+
+        if 'user_mentions' in ent_dict:
+            t_mentions = ent_dict.get('user_mentions')
+            tweet_mentions = mention_hydrate(t_mentions) #
+            tweet_mentions = makeitastring(tweet_mentions)
+            
+        else:
+            tweet_mentions = None
+                
+                
+        if 'hashtags' in ent_dict: 
+            t_hashtags = ent_dict.get('hashtags')  
+            tweet_hashtags = hashtag_hydrate(t_hashtags) 
+            tweet_hashtags = makeitastring(tweet_hashtags)
+                
+        else:
+            tweet_hashtags = None
+
+        # if place_dict:
+        #     t_place = place_dict.get('full_name')   
+        #     tweet_place = place_hydrate(t_place) 
+        #     tweet_place = makeitastring(tweet_place)
+              
+        # else:
+        #     tweet_place = None
+
+        if 'urls' in ent_dict:
+            t_urls = ent_dict.get('urls')
+            tweet_urls = url_hydrate(t_urls) 
+            tweet_urls = makeitastring(tweet_urls)
+        else:
+            tweet_urls = None     
+        
+
         tweet_source = status.source
         tweet_source_url = status.source_url
         tweet_in_reply_to_status_id = status.in_reply_to_status_id
-        tweet_in_reply_to_user_id = status.in_reply_to_user_id
+        tweet_in_response_to_user_id = status.in_reply_to_user_id
         tweet_in_reply_to_screen_name = status.in_reply_to_screen_name
         tweet_username = status.user.screen_name
+        tweet_user_location = status.user.location
         tweet_user = status.user
         tweet_geo = status.geo
         tweet_coordinates = status.coordinates
         tweet_place = status.place
-        tweet_coordinates = status.coordinates
         tweet_is_quote_status = status.is_quote_status
         tweet_retweet_count = status.retweet_count
-        tweet_favorited_count = status.favorite_count
+        tweet_like_count = status.favorite_count
         tweet_favorited = status.favorited
         tweet_retweeted = status.retweeted
         tweet_lang = status.lang
         tweet_created_at = status.created_at
-        print(tweet_username)
-        print(tweet_text)
-        
-        # fields_data = ([tweet_id, tweet_text, tweet_entities, tweet_source, tweet_source_url, tweet_in_reply_to_status_id, tweet_in_reply_to_user_id, tweet_in_reply_to_screen_name, tweet_username,, tweet_user, tweet_geo, tweet_coordinates, tweet_place, tweet_coordinates, tweet_is_quote_status, tweet_retweet_count, tweet_favorited_count, tweet_favorited, tweet_retweted, tweet_lang, tweet_created_at])
-
-                
-        # with open('OutputStreaming.txt', 'a') as f:
-        #     writer = csv.writer(f)
-        #     writer.writerow(fields)
-        
-        # new_row = {"tweet_id":tweet_id, "tweet_text":tweet_text, "tweet_entities":tweet_entities, "tweet_source":tweet_source, "tweet_source_url":tweet_source_url, "tweet_in_reply_to_status_id":tweet_in_reply_to_status_id, "tweet_in_reply_to_user_id":tweet_in_reply_to_user_id, "tweet_in_reply_to_screen_name":tweet_in_reply_to_screen_name, "tweet_username":tweet_username, "tweet_user":tweet_user, "tweet_geo":tweet_geo, "tweet_coordinates":tweet_coordinates, "tweet_place":tweet_place, "tweet_coordinates":tweet_coordinates, "tweet_is_quote_status":tweet_is_quote_status, "tweet_retweet_count":tweet_retweet_count, "tweet_favorited_count":tweet_favorited_count, "tweet_favorited":tweet_favorited, "tweet_retweeted":tweet_retweeted, "tweet_lang":tweet_lang, "tweet_created_at":tweet_created_at}
-        
-        #df = df.append(new_row, ignore_index=True)
+        tweet_user_description = status.user.description
+        tweet_clean_text = clean_tweets(tweet_text) #
+        tweet_sentiment_all = tweet_sentiment_analyzer(tweet_clean_text) #
+        tweet_sentiment_compound = tweet_sentiment_all.get('compound') # 
+        print(tweet_text)  
+        print(tweet_retweeted)
         
 
-        # df.to_csv('streaming2.csv', mode='a')
+        print('\n')
+
         
-        #del df
+        #checking for tweet_id since it is primary key
+        crsr.execute(
+            "SELECT tweet_id, COUNT(*) FROM tweet_all_up WHERE tweet_id = ? GROUP BY tweet_id",
+            (tweet_id)
+        )
+        results = crsr.fetchall()
+        row_count = crsr.rowcount
+        # print("number of affected rows: {}".format(row_count))
+        if row_count == 0:
+            #print("It Does Not Exist")    
+            count = crsr.execute("""
+            INSERT INTO TWEET_ALL_UP (tweet_id, tweet_text, tweet_source, tweet_source_url, tweet_in_reply_to_status_id, tweet_in_reply_to_screen_name, tweet_username, tweet_geo, tweet_coordinates, tweet_is_quote_status, tweet_retweet_count, tweet_like_count, tweet_favorited, tweet_retweeted, tweet_lang, tweet_created_at, query, project, tweet_mentions, tweet_hashtags, tweet_urls, tweet_user_description, jobtype, tweet_clean_text, tweet_sentiment_compound) 
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
+            tweet_id, tweet_text, tweet_source, tweet_source_url, tweet_in_reply_to_status_id, tweet_in_reply_to_screen_name, tweet_username, tweet_geo, tweet_coordinates, tweet_is_quote_status, tweet_retweet_count, tweet_like_count, tweet_favorited, tweet_retweeted, tweet_lang, tweet_created_at, query, project, tweet_mentions, tweet_hashtags, tweet_urls, tweet_user_description, jobtype, tweet_clean_text, tweet_sentiment_compound).rowcount
+
+            #removing tweet_entities, tweet_user (can pull individual attributes), tweet_in_response_to_user_id (dict can't be inserted into SQL), tweet_place
+
+        #print('Rows inserted: ' + str(count))
+            
+        crsr.commit()
+
+        # cnxn.close()
+
+
 
 printer = IDPrinter(
   API_KEY, API_SECRET_KEY,
   ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 )
 
-# with open('OutputStreaming.txt', 'w') as f:
-# writer = csv.writer(f)
-# writer.writerow(["tweet_id", "tweet_text", "tweet_entities", "tweet_source", "tweet_source_url", "tweet_in_reply_to_status_id",        "tweet_in_reply_to_user_id", "tweet_in_reply_to_screen_name", "tweet_username,", "tweet_user", "tweet_geo", "tweet_coordinates", "tweet_place", "tweet_coordinates", "tweet_is_quote_status", "tweet_retweet_count", "tweet_favorited_count", "tweet_favorited", "tweet_retweted", "tweet_lang", "tweet_created_at"])
+
+driver = '{ODBC Driver 17 for SQL Server}'
+server_name = 'twitpoli1984-sqlsrv'
+database_name = 'mosenatetweets-db'
+server = '{server_name}.database.windows.net,1433'.format(server_name=server_name)
+username = "joewils"
+password = "Pissyduck113!@"
+
+connection_string = textwrap.dedent('''
+    Driver={driver};
+    Server={server};
+    Database={database};
+    Uid={username};
+    Pwd={password};
+    Encrypt=yes;
+    TrustServerCertificate=no;
+    Connection Timeout=30;
+'''.format(
+        driver=driver,
+        server=server,
+        database=database_name,
+        username=username,
+        password=password
+))
+
+cnxn: pyodbc.Connection = pyodbc.connect(connection_string)
+crsr: pyodbc.Cursor = cnxn.cursor()
 
 
-printer.filter(track=['hostage'],languages=["en"])
+def place_hydrate(entity_list):  
+    x = 0 #list index
+    i = 1 #list length
+    container = []
+    entity_length = len(entity_list)
+    while i <= entity_length:
+        temp_list = entity_list[x]
+        t = temp_list.get("full_name")
+        container.append(t)
+        i = i + 1
+        x = x + 1  
+    return container
 
+def mention_hydrate(entity_list):  
+    x = 0 #list index
+    i = 1 #list length
+    container = []
+    entity_length = len(entity_list)
+    while i <= entity_length:
+        temp_list = entity_list[x]
+        j = temp_list.get("screen_name")
+        container.append(j)
+        i = i + 1
+        x = x + 1   
+    return container
 
+def hashtag_hydrate(entity_list):  
+    x = 0 #list index
+    i = 1 #list length
+    container = []
+    entity_length = len(entity_list)
+    while i <= entity_length:
+        temp_list = entity_list[x]
+        j = temp_list.get("text")
+        container.append(j)
+        i = i + 1
+        x = x + 1  
+    return container
 
+def url_hydrate(entity_list):  
+    x = 0 #list index
+    i = 1 #list length
+    container = []
+    entity_length = len(entity_list)
+    while i <= entity_length:
+        temp_list = entity_list[x]
+        j = temp_list.get("expanded_url")
+        container.append(j)
+        i = i + 1
+        x = x + 1  
+    return container
 
-#################################################################################################
+def makeitastring(wannabestring):
+    convertedstring = ','.join(map(str, wannabestring))
+    return(convertedstring)
+
+def remove_whitespace(text):
+    return  " ".join(text.split())
+
+def clean_tweets(tweet_text):
+  p.set_options(p.OPT.URL, p.OPT.MENTION)
+  clean_tweet_text = p.clean(tweet_text)
+  #clean_tweet_text = p.parse(clean_text)
+  clean_tweeet_text = remove_whitespace(clean_tweet_text)
+  return(clean_tweet_text)
+
+#creates a dictionary of positive score, neutral score, negativie score or compound score. Here it is 
+def tweet_sentiment_analyzer(clean_text):
+  sentiment_scores = []
+  sentiment_scores = analyzer.polarity_scores(clean_text)
+  return(sentiment_scores)
+
+ent_dict = []
+analyzer = SentimentIntensityAnalyzer()
+
+################################################################################################################################
+printer.filter(track=['covid'],languages=["en"])
+################################################################################################################################
